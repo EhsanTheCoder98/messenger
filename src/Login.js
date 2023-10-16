bc5ccad3014ade534e4c4a299c07b766f1e2f585
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-10-16 18:17:09 +0330

    some visual changes

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ import { auth } from './firebase';
 const Login = () => {
     return (
         <div className={styles.container}>
-                <h2>Welcom to IChat!</h2>
+                <h2>Welcome to IChat!</h2>
                 <h3 onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>Login with Google Account</h3>
         </div>
     );
